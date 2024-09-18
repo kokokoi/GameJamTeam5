4b934a9cf33a468ab1fcbf04993ab1338b4f7fcd
OYUNBAYAR OYUN-ERDENE <2230271@ecc.ac.jp>
2024-09-18 17:34:11 +0900

    PlayerControllerUpdate

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,6 +9,13 @@ public class PlayerController : MonoBehaviour
     //現在のスピードを保持しておく本数
     float currentSpeed;
 
+    public enum Button
+    {
+        Right = 0, Left, Up, Down, Shift, Space, Max
+    }
+
+    public UIButton[] uiButton = new UIButton[(int)Button.Max];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,9 +40,64 @@ public class PlayerController : MonoBehaviour
         position.y = position.y + 8.0f * jump * Time.deltaTime;
 
 
-
         transform.position = position;
 
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        ApplyDownArrow();
+        ApplyUpArrow();
+        ApplyRightArrow();
+        ApplyLeftArrow();
+        ApplyShiftKey();
+        ApplySpaceKey();
+
+
+    }
+
+    void ApplyDownArrow()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            uiButton[(int)Button.Down].ButtonDown();
+        else
+            uiButton[(int)Button.Down].ButtonRelease();
+    }
+    void ApplyUpArrow()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            uiButton[(int)Button.Up].ButtonDown();
+        else
+            uiButton[(int)Button.Up].ButtonRelease();
+    }
+    void ApplyRightArrow()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            uiButton[(int)Button.Right].ButtonDown();
+        else
+            uiButton[(int)Button.Right].ButtonRelease();
+    }
+    void ApplyLeftArrow()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            uiButton[(int)Button.Left].ButtonDown();
+        else
+            uiButton[(int)Button.Left].ButtonRelease();
+    }
+    void ApplyShiftKey()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            uiButton[(int)Button.Shift].ButtonDown();
+        else
+            uiButton[(int)Button.Shift].ButtonRelease();
+    }
+    void ApplySpaceKey()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            uiButton[(int)Button.Space].ButtonDown();
+        else
+            uiButton[(int)Button.Space].ButtonRelease();
     }
 
 }
